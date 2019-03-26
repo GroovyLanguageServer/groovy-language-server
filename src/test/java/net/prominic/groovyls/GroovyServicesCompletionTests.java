ece8026ee5bc78c03ee54e8f67f4736d930d5927
Josh Tynjala <joshtynjala@gmail.com>
2019-03-26 14:35:40 -0700

    some tests for completion and signature help

---
+++ src/test/java/net/prominic/groovyls/GroovyServicesCompletionTests.java
@@ -0,0 +1,141 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Prominic.NET, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License
+// 
+// Author: Prominic.NET, Inc.
+// No warranty of merchantability or fitness of any kind. 
+// Use this software at your own risk.
+////////////////////////////////////////////////////////////////////////////////
+package net.prominic.groovyls;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import org.eclipse.lsp4j.CompletionItem;
+import org.eclipse.lsp4j.CompletionItemKind;
+import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.CompletionParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.MessageActionItem;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4j.ShowMessageRequestParams;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GroovyServicesCompletionTests {
+	private static final String LANGUAGE_GROOVY = "groovy";
+
+	private GroovyServices services;
+	private Path workspaceRoot;
+
+	@BeforeEach
+	void setup() {
+		workspaceRoot = Paths.get("./test_workspace");
+		services = new GroovyServices();
+		services.setWorkspaceRoot(workspaceRoot);
+		services.connect(new LanguageClient() {
+
+			@Override
+			public void telemetryEvent(Object object) {
+
+			}
+
+			@Override
+			public CompletableFuture<MessageActionItem> showMessageRequest(ShowMessageRequestParams requestParams) {
+				return null;
+			}
+
+			@Override
+			public void showMessage(MessageParams messageParams) {
+
+			}
+
+			@Override
+			public void publishDiagnostics(PublishDiagnosticsParams diagnostics) {
+
+			}
+
+			@Override
+			public void logMessage(MessageParams message) {
+
+			}
+		});
+	}
+
+	@AfterEach
+	void tearDown() {
+		services = null;
+	}
+
+	@Test
+	void testMemberAccessOnLocalVariable() throws Exception {
+		Path filePath = workspaceRoot.resolve("./src/main/java/Completion.groovy");
+		String uri = filePath.toUri().toString();
+		StringBuilder contents = new StringBuilder();
+		contents.append("class Completion {\n");
+		contents.append("  public Completion() {\n");
+		contents.append("    String localVar\n");
+		contents.append("    localVar.\n");
+		contents.append("  }\n");
+		contents.append("}");
+		TextDocumentItem textDocumentItem = new TextDocumentItem(uri, LANGUAGE_GROOVY, 1, contents.toString());
+		services.didOpen(new DidOpenTextDocumentParams(textDocumentItem));
+		TextDocumentIdentifier textDocument = new TextDocumentIdentifier(uri);
+		Position position = new Position(3, 13);
+		Either<List<CompletionItem>, CompletionList> result = services
+				.completion(new CompletionParams(textDocument, position)).get();
+		Assertions.assertTrue(result.isLeft());
+		List<CompletionItem> items = result.getLeft();
+		List<CompletionItem> filteredItems = items.stream().filter(item -> {
+			return item.getLabel().equals("charAt") && item.getKind().equals(CompletionItemKind.Method);
+		}).collect(Collectors.toList());
+		Assertions.assertTrue(filteredItems.size() >= 1);
+	}
+
+	@Test
+	void testMemberAccessOnMemberVariable() throws Exception {
+		Path filePath = workspaceRoot.resolve("./src/main/java/Completion.groovy");
+		String uri = filePath.toUri().toString();
+		StringBuilder contents = new StringBuilder();
+		contents.append("class Completion {\n");
+		contents.append("  String memberVar\n");
+		contents.append("  public Completion() {\n");
+		contents.append("    memberVar.\n");
+		contents.append("  }\n");
+		contents.append("}");
+		TextDocumentItem textDocumentItem = new TextDocumentItem(uri, LANGUAGE_GROOVY, 1, contents.toString());
+		services.didOpen(new DidOpenTextDocumentParams(textDocumentItem));
+		TextDocumentIdentifier textDocument = new TextDocumentIdentifier(uri);
+		Position position = new Position(3, 14);
+		Either<List<CompletionItem>, CompletionList> result = services
+				.completion(new CompletionParams(textDocument, position)).get();
+		Assertions.assertTrue(result.isLeft());
+		List<CompletionItem> items = result.getLeft();
+		List<CompletionItem> filteredItems = items.stream().filter(item -> {
+			return item.getLabel().equals("charAt") && item.getKind().equals(CompletionItemKind.Method);
+		}).collect(Collectors.toList());
+		Assertions.assertTrue(filteredItems.size() >= 1);
+	}
+}
---
+++ src/test/java/net/prominic/groovyls/GroovyServicesSignatureHelpTests.java
@@ -0,0 +1,180 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2019 Prominic.NET, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License
+// 
+// Author: Prominic.NET, Inc.
+// No warranty of merchantability or fitness of any kind. 
+// Use this software at your own risk.
+////////////////////////////////////////////////////////////////////////////////
+package net.prominic.groovyls;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.MessageActionItem;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.ParameterInformation;
+import org.eclipse.lsp4j.Position;
+import org.eclipse.lsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4j.ShowMessageRequestParams;
+import org.eclipse.lsp4j.SignatureHelp;
+import org.eclipse.lsp4j.SignatureInformation;
+import org.eclipse.lsp4j.TextDocumentIdentifier;
+import org.eclipse.lsp4j.TextDocumentItem;
+import org.eclipse.lsp4j.TextDocumentPositionParams;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GroovyServicesSignatureHelpTests {
+	private static final String LANGUAGE_GROOVY = "groovy";
+
+	private GroovyServices services;
+	private Path workspaceRoot;
+
+	@BeforeEach
+	void setup() {
+		workspaceRoot = Paths.get("./test_workspace");
+		services = new GroovyServices();
+		services.setWorkspaceRoot(workspaceRoot);
+		services.connect(new LanguageClient() {
+
+			@Override
+			public void telemetryEvent(Object object) {
+
+			}
+
+			@Override
+			public CompletableFuture<MessageActionItem> showMessageRequest(ShowMessageRequestParams requestParams) {
+				return null;
+			}
+
+			@Override
+			public void showMessage(MessageParams messageParams) {
+
+			}
+
+			@Override
+			public void publishDiagnostics(PublishDiagnosticsParams diagnostics) {
+
+			}
+
+			@Override
+			public void logMessage(MessageParams message) {
+
+			}
+		});
+	}
+
+	@AfterEach
+	void tearDown() {
+		services = null;
+	}
+
+	@Test
+	void testSignatureHelpOnMethod() throws Exception {
+		Path filePath = workspaceRoot.resolve("./src/main/java/Completion.groovy");
+		String uri = filePath.toUri().toString();
+		StringBuilder contents = new StringBuilder();
+		contents.append("class SignatureHelp {\n");
+		contents.append("  public SignatureHelp() {\n");
+		contents.append("    method(\n");
+		contents.append("  }\n");
+		contents.append("  public void method(int param0) {}\n");
+		contents.append("}");
+		TextDocumentItem textDocumentItem = new TextDocumentItem(uri, LANGUAGE_GROOVY, 1, contents.toString());
+		services.didOpen(new DidOpenTextDocumentParams(textDocumentItem));
+		TextDocumentIdentifier textDocument = new TextDocumentIdentifier(uri);
+		Position position = new Position(2, 11);
+		SignatureHelp signatureHelp = services.signatureHelp(new TextDocumentPositionParams(textDocument, position))
+				.get();
+		List<SignatureInformation> signatures = signatureHelp.getSignatures();
+		Assertions.assertEquals(1, signatures.size());
+		SignatureInformation signature = signatures.get(0);
+		Assertions.assertEquals("public void method(int param0)", signature.getLabel());
+		List<ParameterInformation> params = signature.getParameters();
+		Assertions.assertEquals(1, params.size());
+		ParameterInformation param0 = params.get(0);
+		Assertions.assertEquals("int param0", param0.getLabel());
+		Assertions.assertEquals((int) 0, (int) signatureHelp.getActiveSignature());
+		Assertions.assertEquals((int) 0, (int) signatureHelp.getActiveParameter());
+	}
+
+	@Test
+	void testSignatureHelpOnMethodWithMultipleParameters() throws Exception {
+		Path filePath = workspaceRoot.resolve("./src/main/java/Completion.groovy");
+		String uri = filePath.toUri().toString();
+		StringBuilder contents = new StringBuilder();
+		contents.append("class SignatureHelp {\n");
+		contents.append("  public SignatureHelp() {\n");
+		contents.append("    method(\n");
+		contents.append("  }\n");
+		contents.append("  public void method(int param0, String param1) {}\n");
+		contents.append("}");
+		TextDocumentItem textDocumentItem = new TextDocumentItem(uri, LANGUAGE_GROOVY, 1, contents.toString());
+		services.didOpen(new DidOpenTextDocumentParams(textDocumentItem));
+		TextDocumentIdentifier textDocument = new TextDocumentIdentifier(uri);
+		Position position = new Position(2, 11);
+		SignatureHelp signatureHelp = services.signatureHelp(new TextDocumentPositionParams(textDocument, position))
+				.get();
+		List<SignatureInformation> signatures = signatureHelp.getSignatures();
+		Assertions.assertEquals(1, signatures.size());
+		SignatureInformation signature = signatures.get(0);
+		Assertions.assertEquals("public void method(int param0, String param1)", signature.getLabel());
+		List<ParameterInformation> params = signature.getParameters();
+		Assertions.assertEquals(2, params.size());
+		ParameterInformation param0 = params.get(0);
+		Assertions.assertEquals("int param0", param0.getLabel());
+		ParameterInformation param1 = params.get(1);
+		Assertions.assertEquals("String param1", param1.getLabel());
+		Assertions.assertEquals((int) 0, (int) signatureHelp.getActiveSignature());
+		Assertions.assertEquals((int) 0, (int) signatureHelp.getActiveParameter());
+	}
+
+	@Test
+	void testSignatureHelpOnMethodWithActiveParameter() throws Exception {
+		Path filePath = workspaceRoot.resolve("./src/main/java/Completion.groovy");
+		String uri = filePath.toUri().toString();
+		StringBuilder contents = new StringBuilder();
+		contents.append("class SignatureHelp {\n");
+		contents.append("  public SignatureHelp() {\n");
+		contents.append("    method(123,\n");
+		contents.append("  }\n");
+		contents.append("  public void method(int param0, String param1) {}\n");
+		contents.append("}");
+		TextDocumentItem textDocumentItem = new TextDocumentItem(uri, LANGUAGE_GROOVY, 1, contents.toString());
+		services.didOpen(new DidOpenTextDocumentParams(textDocumentItem));
+		TextDocumentIdentifier textDocument = new TextDocumentIdentifier(uri);
+		Position position = new Position(2, 15);
+		SignatureHelp signatureHelp = services.signatureHelp(new TextDocumentPositionParams(textDocument, position))
+				.get();
+		List<SignatureInformation> signatures = signatureHelp.getSignatures();
+		Assertions.assertEquals(1, signatures.size());
+		SignatureInformation signature = signatures.get(0);
+		Assertions.assertEquals("public void method(int param0, String param1)", signature.getLabel());
+		List<ParameterInformation> params = signature.getParameters();
+		Assertions.assertEquals(2, params.size());
+		ParameterInformation param0 = params.get(0);
+		Assertions.assertEquals("int param0", param0.getLabel());
+		ParameterInformation param1 = params.get(1);
+		Assertions.assertEquals("String param1", param1.getLabel());
+		Assertions.assertEquals((int) 0, (int) signatureHelp.getActiveSignature());
+		Assertions.assertEquals((int) 1, (int) signatureHelp.getActiveParameter());
+	}
+}
