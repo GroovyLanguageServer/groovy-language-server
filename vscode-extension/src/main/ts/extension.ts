86f4d6f1f56cd3af6144a3e09f5115cfb76e7181
Rich C <richcariens@gmail.com>
2021-02-27 11:42:25 -0500

    extension promise should be void

--- vscode-extension/src/main/ts/extension.ts
+++ vscode-extension/src/main/ts/extension.ts
@@ -94,7 +94,7 @@ function startLanguageServer() {
   vscode.window.withProgress(
     { location: vscode.ProgressLocation.Window },
     (progress) => {
-      return new Promise((resolve, reject) => {
+      return new Promise<void>((resolve, reject) => {
         if (!extensionContext) {
           //something very bad happened!
           resolve();
